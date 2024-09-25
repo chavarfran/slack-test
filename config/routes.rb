743319f9f6d23d13e3698ddc015258ef2895c72d
Francisco Chavarria <fchavarriar@outlook.com>
2024-09-25 08:13:16 -0600

    ✨ Feat: new featas

--- config/routes.rb
+++ config/routes.rb
@@ -9,4 +9,7 @@ Rails.application.routes.draw do
   root 'welcome#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get "about",      to: "page#about"
+  get "contact",    to: "page#contact"
+
 end
